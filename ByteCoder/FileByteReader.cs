3ebb53920736a774fd5b97e75567d7840b23d580
Alexander Vovnenko <vovnenko_ne@mail.ru>
2016-01-27 02:05:09 +0300

    code refactoring. add test project.

---
+++ ByteCoder/FileByteReader.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace ByteCoder
+{
+    public class FileByteReader : IDisposable
+    {
+        private const int BufferSize = 4096 * 4096;
+        private const int SizeOfWord = sizeof(UInt32);
+
+        private readonly Stream stream;
+
+        public FileByteReader(string filePath)
+        {
+            stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        }
+
+        public void Dispose()
+        {
+            stream.Dispose();
+        }
+
+        public BigInteger CalculateChecksum()
+        {
+            if (!stream.CanSeek) return 0;
+
+            int top = Console.CursorTop;
+            stream.Seek(0, SeekOrigin.Begin);
+            
+            BigInteger checksum = 0;
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                byte[] buffer = new byte[BufferSize];
+                while (reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                    int length = reader.Read(buffer, 0, buffer.Length);
+
+                    for (int i = 0; i < length; i += SizeOfWord)
+                    {
+                        if (length - i < SizeOfWord)
+                        {
+                            for (int j = length; j < buffer.Length && j < i + SizeOfWord; j++)
+                            {
+                                buffer[j] = 0;
+                            }
+                        }
+                        checksum += BitConverter.ToUInt32(buffer, i);
+                    }
+
+                    Console.SetCursorPosition(0, top);
+                    Console.Write("{0}%..", getPositionPercent(reader.BaseStream));
+                }
+
+            }
+
+            return checksum;
+        }
+
+
+        public List<long> FindAll(string value)
+        {
+            if (!stream.CanSeek) return new List<long>();
+            
+            int top = Console.CursorTop;
+            stream.Seek(0, SeekOrigin.Begin);
+
+            List<TestOccurrence> testOccurrences = new List<TestOccurrence>();
+            List<long> occurrences = new List<long>();
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                char[] buffer = new char[BufferSize];
+                while (reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                    int readedLength = reader.ReadBlock(buffer, 0, buffer.Length);
+
+                    for (int i = 0; i < readedLength; i++)
+                    {
+                        checkTestOccurrences(value, testOccurrences, occurrences, i, buffer);
+
+                        if (buffer[i] == value[0])
+                        {
+                            testOccurrences.Add(new TestOccurrence
+                            {
+                                Position = reader.BaseStream.Position - reader.CurrentEncoding.GetByteCount(buffer, i, readedLength - i)
+                            });
+                        }
+                    }
+
+                    Console.SetCursorPosition(0, top);
+                    Console.Write("{0}%..", getPositionPercent(reader.BaseStream));
+                }
+                checkTestOccurrences(value, testOccurrences, occurrences, buffer.Length - 1, buffer);
+            }
+           
+            Console.SetCursorPosition(0, top);
+            return occurrences;
+        }
+
+
+        private static void checkTestOccurrences(string value,
+           List<TestOccurrence> testOccurrences,
+           List<long> occurrences,
+           int bufferIndex,
+           char[] buffer)
+        {
+            for (int i = 0; i < testOccurrences.Count; )
+            {
+                testOccurrences[i].CurrentKeyIndex++;
+                if (testOccurrences[i].CurrentKeyIndex >= value.Length)
+                {
+                    occurrences.Add(testOccurrences[i].Position);
+                    testOccurrences.RemoveAt(i);
+                }
+                else
+                {
+                    if (buffer[bufferIndex] == value[testOccurrences[i].CurrentKeyIndex])
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        testOccurrences.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        private static int getPositionPercent(Stream stream)
+        {
+            return (int)(Math.Floor(((double)stream.Position / stream.Length) * 100));
+        }
+        
+        private class TestOccurrence
+        {
+            public long Position;
+            public int CurrentKeyIndex;
+        }
+    }
+}
---
+++ ByteCoderTests/FileByteReaderTests.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using ByteCoder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ByteCoderTests
+{
+    [TestClass]
+    public class FileByteReaderTests
+    {
+        Random random = new Random();
+        private const string TmpFileName = "justForTesting.test";
+        
+
+        [TestMethod]
+        public void EmptyFileTest()
+        {
+            createFile(string.Empty);
+            
+            using (FileByteReader reader = new FileByteReader(TmpFileName))
+            {
+                Assert.AreEqual(0, reader.CalculateChecksum());
+                List<long> occurences = reader.FindAll(string.Empty);
+                Assert.IsNotNull(occurences);
+                Assert.AreEqual(0, occurences.Count);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void NoFileExceptionTest()
+        {
+            using (FileByteReader reader = new FileByteReader(TmpFileName))
+            {
+            }
+        }
+
+        [TestMethod]
+        public void ChecksumTest()
+        {
+            BigInteger expectedSum = createFileWithRandomNumbers(15);
+
+            assertReaderSum(expectedSum);
+        }
+
+        [TestMethod]
+        public void ChecksumTestWithCutEnding()
+        {
+            BigInteger expectedSum = createFileWithRandomNumbers(10, true);
+
+            assertReaderSum(expectedSum);
+        }
+
+
+        [TestMethod]
+        public void ChecksumTestBigFile()
+        {
+            BigInteger expectedSum = createFileWithRandomNumbers(1000000);
+
+            assertReaderSum(expectedSum);
+        }
+
+        [TestMethod]
+        public void FindAllCustomTest()
+        {
+            string searchString = "hello";
+            List<long> expectedMatches = createFileWithStringInsertions(searchString);
+
+
+            using (FileByteReader reader = new FileByteReader(TmpFileName))
+            {
+                List<long> matches = reader.FindAll(searchString);
+                Assert.AreEqual(expectedMatches.Count, matches.Count);
+
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    Assert.AreEqual(expectedMatches[i], matches[i]);
+                }
+            }
+
+            
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+
+            removeFile();
+        }
+
+        private List<long> createFileWithStringInsertions(string searchString)
+        {
+            List<long> expectedMatches = new List<long>();
+            int fileSize = 10000;
+            int matchesCount = 12;
+            int shift = fileSize/matchesCount;
+            using (StreamWriter writer = new StreamWriter(TmpFileName))
+            {
+                for (int i = 0; i < fileSize; i++)
+                {
+                    writer.Write('a');
+                }
+            }
+            using (FileStream file = new FileStream(TmpFileName, FileMode.OpenOrCreate))
+            {
+                for (int i = 0; i < matchesCount; i++)
+                {
+                    int offset = random.Next(shift*i, shift*(i + 1));
+                    file.Seek(offset, SeekOrigin.Begin);
+                    file.Write(Encoding.UTF8.GetBytes(searchString), 0, searchString.Length);
+                    expectedMatches.Add(offset);
+                }
+            }
+            return expectedMatches;
+        }
+
+        private void assertReaderSum(BigInteger realSum)
+        {
+            using (FileByteReader reader = new FileByteReader(TmpFileName))
+            {
+                Assert.AreEqual(realSum, reader.CalculateChecksum());
+            }
+        }
+
+        private BigInteger createFileWithRandomNumbers(int count, bool withCutEnding = false)
+        {
+            BigInteger sum = 0;
+            using(FileStream file = new FileStream(TmpFileName, FileMode.Create))
+            {
+                using (BinaryWriter writer = new BinaryWriter(file))
+                {
+                    byte[] buffer = new byte[4096*4096];
+                    int cur = 0;
+                    for (int i = 0; i < count; i++)
+                    {
+                        uint nextNumber = getRandomUInt();
+                        sum += nextNumber;
+                        byte[] bytes = BitConverter.GetBytes(nextNumber);
+                        for (int j = 0; j < bytes.Length; j++)
+                        {
+                            buffer[cur + j] = bytes[j];
+                        }
+                        cur += bytes.Length;
+
+                        if (cur >= buffer.Length)
+                        {
+                            writer.Write(buffer);
+                            for (int j = 0; j < buffer.Length; j++)
+                            {
+                                buffer[j] = 0;
+                            }
+                            cur = 0;
+                        }
+                    }
+
+                    if (cur < buffer.Length)
+                    {
+                        writer.Write(buffer, 0, cur);
+                    }
+
+                    if (withCutEnding)
+                    {
+                        byte[] bytes = BitConverter.GetBytes(getRandomUInt());
+                        bytes[2] = 0;
+                        bytes[3] = 0;
+                        sum += BitConverter.ToUInt32(bytes, 0);
+                        writer.Write(bytes, 0, 2);
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private uint getRandomUInt()
+        {
+            return (uint)(random.NextDouble() * uint.MaxValue);
+        }
+
+        private void createFile(string text)
+        {
+            using (StreamWriter writer = new StreamWriter(TmpFileName))
+            {
+                writer.Write(text);
+            }
+        }
+
+        private void removeFile()
+        {
+            if (File.Exists(TmpFileName))
+                File.Delete(TmpFileName);
+        }
+
+    }
+}
